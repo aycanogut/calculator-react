96d39cd22b6fbc1239820407f52dc818684fd1a1
aycanogut <aycanonfire@gmail.com>
2021-12-21 16:10:04 +0300

    add colors

--- src/styles/helpers.js
+++ src/styles/helpers.js
@@ -8,6 +8,7 @@ const size = {
   xxl: '1400px'
 };
 
+// device sizes
 const device = {
   xs: `(min-width: ${size.xs})`,
   sm: `(min-width: ${size.sm})`,
@@ -15,6 +16,17 @@ const device = {
   lg: `(min-width: ${size.lg})`,
   xl: `(min-width: ${size.xl})`,
   xxl: `(min-width: ${size.xxl})`
+};
+
+// colors
+const colors = {
+  textPrimary: '#ffffff',
+  textSecondary: '#9b9b9c',
+  background: '#2b2c32',
+  input: '#1d1d1d',
+  numberKeys: '#070707',
+  operatorKeys: '#141414',
+  equalKey: '#463f66'
 };
 
 // typography
