8c4f52d2cffb0a4a74af8e02e3a4bcb726407c0a
aycanogut <aycanonfire@gmail.com>
2021-12-21 15:52:23 +0300

    add helper files

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,13 @@
+// globalStyles.js
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+  html {
+      font-size: 62.5%;
+      box-sizing: border-box;
+  }
+
+`;
+
+export default GlobalStyle;
---
+++ src/styles/helpers.js
@@ -0,0 +1,29 @@
+// breakpoints
+const size = {
+  xs: '320px',
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1200px',
+  xxl: '1400px'
+};
+
+const device = {
+  xs: `(min-width: ${size.xs})`,
+  sm: `(min-width: ${size.sm})`,
+  md: `(min-width: ${size.md})`,
+  lg: `(min-width: ${size.lg})`,
+  xl: `(min-width: ${size.xl})`,
+  xxl: `(min-width: ${size.xxl})`
+};
+
+// typography
+const fontWeight = {
+  thin: '300',
+  regular: '400',
+  semiBold: '600',
+  bold: '700',
+  extraBold: '800'
+};
+
+export default { size, device, fontWeight };
