30de5522163bdfb7e362e1a0488f7659430dba14
aycanogut <aycanonfire@gmail.com>
2021-12-22 21:26:35 +0300

    add styled-reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import GlobalStyle from './styles/globalStyles';
+import { Reset } from 'styled-reset';
 
 function App() {
   return (
     <>
+      <Reset />
       <GlobalStyle />
     </>
   );
