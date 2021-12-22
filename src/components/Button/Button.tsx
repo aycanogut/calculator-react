f722ed455977c256bd267d10ad7dffb51865c439
aycanogut <aycanonfire@gmail.com>
2021-12-22 20:09:06 +0300

    add components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 
 const Button = ({ children }: { children: HTMLElement }) => {
-  const style = {
-    backgroundColor: 'red',
-    color: 'white',
-    cursor: 'pointer',
-    padding: '10px'
-  };
   return <button>{children}</button>;
 };
 
