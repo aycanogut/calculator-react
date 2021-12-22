dfb81cff3c6e90b75b61637b11e1a6d9843ba3f6
aycanogut <aycanonfire@gmail.com>
2021-12-22 21:35:25 +0300

    add wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import GlobalStyle from './styles/globalStyles';
 import { Reset } from 'styled-reset';
 
+import Wrapper from './components/Wrapper';
+
 function App() {
   return (
     <>
       <Reset />
       <GlobalStyle />
+      <Wrapper />
     </>
   );
 }
