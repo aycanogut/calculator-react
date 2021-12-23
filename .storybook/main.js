9327993b096e8adc5930571638f3ed24eb2d0f99
aycanogut <aycanonfire@gmail.com>
2021-12-23 13:24:16 +0300

    add actions and docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,8 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app'
+    '@storybook/addon-actions',
+    '@storybook/addon-docs',
   ],
   framework: '@storybook/react',
   core: {
