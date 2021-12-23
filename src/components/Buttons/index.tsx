61aeff19e78eecb90a709efc9b6283cd2d91c842
aycanogut <aycanonfire@gmail.com>
2021-12-23 21:34:00 +0300

    add modifiers

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
-
 import { NumberButton, OperatorButton, EqualButton } from '../Button';
 
 const StyledButtons = styled.section``;
 
-const Buttons = () => {
+const Buttons = ({ modifiers }: { modifiers?: string }) => {
   return (
     <StyledButtons>
-      <NumberButton>1</NumberButton>
-      <OperatorButton>+</OperatorButton>
-      <EqualButton>=</EqualButton>
+      <NumberButton modifiers="number">1</NumberButton>
+      <OperatorButton modifiers="operator">+</OperatorButton>
+      <EqualButton modifiers="equal">=</EqualButton>
     </StyledButtons>
   );
 };
