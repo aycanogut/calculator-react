e1c38fd6b675d3c39d97b0edaff5994aa7a17d11
aycanogut <aycanonfire@gmail.com>
2021-12-23 21:30:12 +0300

    add Button story

--- src/stories/Button.stories.js
+++
@@ -1,8 +0,0 @@
-import Button from '../components/Button';
-
-export const Red = () => <Button backgroundColor="red">Hello Button</Button>;
-
-export default {
-  title: 'Button',
-  component: Button
-};
