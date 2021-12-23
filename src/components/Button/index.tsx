f2c6a3343653064578c0e384abfcbfc0cafea5aa
aycanogut <aycanonfire@gmail.com>
2021-12-23 13:01:09 +0300

    add styles and props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import helpers from '../../styles/helpers';
+
 const StyledButton = styled.button`
-  background-color: gray;
-  color: white;
+  display: inline-block;
+  padding: 1.4rem 3.4rem;
+  /* margin: 10rem; */
+  background-color: ${helpers.color.numberKeys};
+  color: ${helpers.color.textPrimary};
+  outline: none;
+  border: none;
+  cursor: pointer;
+  font-size: ${helpers.typography.fontSize.md};
+  font-weight: ${helpers.typography.fontWeight.bold};
+  text-decoration: none;
+  white-space: nowrap;
 `;
 
-const Button = ({ children }: { children: string }) => {
-  return <StyledButton>{children}</StyledButton>;
+const Button = ({ children, ...props }: { children: string }) => {
+  return <StyledButton {...props}>{children}</StyledButton>;
 };
 
 export default Button;
