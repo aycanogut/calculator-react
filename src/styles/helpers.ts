6aacc72505cf48e30745c4f5c107226d290a8b52
aycanogut <aycanonfire@gmail.com>
2021-12-23 12:57:31 +0300

    update color name, add font-size

--- src/styles/helpers.ts
+++ src/styles/helpers.ts
@@ -19,7 +19,7 @@ const device = {
 };
 
 // colors
-const colors = {
+const color = {
   textPrimary: '#ffffff',
   textSecondary: '#9b9b9c',
   background: '#2b2c32',
@@ -30,12 +30,17 @@ const colors = {
 };
 
 // typography
-const fontWeight = {
-  thin: '300',
-  regular: '400',
-  semiBold: '600',
-  bold: '700',
-  extraBold: '800'
+const typography = {
+  fontSize: {
+    md: '1.6rem'
+  },
+  fontWeight: {
+    thin: '300',
+    regular: '400',
+    semiBold: '600',
+    bold: '700',
+    extraBold: '800'
+  }
 };
 
-export default { size, device, fontWeight };
+export default { size, device, color, typography };
