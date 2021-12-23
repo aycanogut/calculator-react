3142c03b0992aff2a0a562d3f6002b8806092c45
aycanogut <aycanonfire@gmail.com>
2021-12-23 21:29:37 +0300

    add storybook addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,18 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app'
-    '@storybook/addon-actions',
-    '@storybook/addon-docs',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-backgrounds/register',
+    '@storybook/addon-contexts/register',
+
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true
+      }
+    }
   ],
   framework: '@storybook/react',
   core: {
