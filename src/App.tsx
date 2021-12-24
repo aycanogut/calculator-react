f5f3dc61b3ac9e15566284b8a265f1a8905eabf5
aycanogut <aycanonfire@gmail.com>
2021-12-24 14:56:11 +0300

    remove globalstyle and reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import GlobalStyle from './styles/globalStyles';
-import { Reset } from 'styled-reset';
 
 import Wrapper from './components/Wrapper';
 
 function App() {
   return (
     <>
-      <Reset />
-      <GlobalStyle />
       <Wrapper />
     </>
   );
