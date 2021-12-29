e32ebc0c05bfb6a62dcb9a88d9ecd2a37bd8b936
aycanogut <aycanonfire@gmail.com>
2021-12-29 17:46:27 +0300

    update component

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './Wrapper.module.css';
-import Calculator from '../Calculator';
+import Buttons from '../Buttons';
 
 const Wrapper = () => {
   return (
     <div className={styles.Wrapper}>
-      <Calculator />
+      <Buttons />
     </div>
   );
 };
