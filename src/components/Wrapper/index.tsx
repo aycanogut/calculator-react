d0a7c2e3b223c1c5618034cbfe8e69a44681a074
Aycan Ogut <aycanonfire@gmail.com>
2022-01-03 23:01:46 +0300

    add history component

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './Wrapper.module.css';
 import Screen from '../Screen';
 import Buttons from '../Buttons';
+import History from '../History';
 
 const Wrapper = () => {
   return (
     <div className={styles.Wrapper}>
       <Screen />
-      <Buttons />
+      {/* <Buttons /> */}
+      <History />
     </div>
   );
 };
