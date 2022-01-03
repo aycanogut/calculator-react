a01c36251aab2bb6d18ffc144a229f179677427c
Aycan Ogut <aycanonfire@gmail.com>
2022-01-03 22:13:28 +0300

    add screen

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './Wrapper.module.css';
+import Screen from '../Screen';
 import Buttons from '../Buttons';
 
 const Wrapper = () => {
   return (
     <div className={styles.Wrapper}>
+      <Screen />
       <Buttons />
     </div>
   );
