dfaebced2c39cbba2528c169f29e7ecd85add088
Aycan Ogut <aycanonfire@gmail.com>
2022-01-04 21:37:27 +0300

    add calculator

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import styles from './Wrapper.module.css';
-import Screen from '../Screen';
-import Buttons from '../Buttons';
+
+import Calculator from '../Calculator';
 import History from '../History';
 
 const Wrapper = () => {
   return (
     <div className={styles.Wrapper}>
-      <Screen />
-      {/* <Buttons /> */}
-      <History />
+      <Calculator />
+      {/* <History /> */}
     </div>
   );
 };
