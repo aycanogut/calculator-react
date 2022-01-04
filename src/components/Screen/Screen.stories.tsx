8a2b911cd482d88525ee9b6e99d264df3e55fe53
Aycan Ogut <aycanonfire@gmail.com>
2022-01-04 21:58:14 +0300

    add wrapper for story

--- src/components/Screen/Screen.stories.tsx
+++ src/components/Screen/Screen.stories.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Screen from '../Screen';
 import '../../styles/global.css';
+import styles from './Screen.module.css';
 
 export default {
   title: 'Screen',
   component: Screen
 };
 
-export const ScreenStory = () => <Screen />;
+export const ScreenStory = () => (
+  <div className={styles.StoryWrapper}>
+    <Screen />
+  </div>
+);
