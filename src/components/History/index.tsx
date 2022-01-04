d99564b0d3c04562f64d853a2dbefd97573e1bef
Aycan Ogut <aycanonfire@gmail.com>
2022-01-04 18:05:18 +0300

    add dummy nodes

--- src/components/History/index.tsx
+++ src/components/History/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './History.module.css';
 
+import HistoryItem from '../HistoryItem';
 import Footer from '../Footer';
 
 const History = () => {
   return (
     <section className={styles.History}>
+      <HistoryItem firstArgument={21} operator="+" secondArgument={42} result={63} />
+      <HistoryItem firstArgument={21} operator="+" secondArgument={42} result={63} />
+      <HistoryItem firstArgument={21} operator="+" secondArgument={42} result={63} />
+
       <Footer />
     </section>
   );
