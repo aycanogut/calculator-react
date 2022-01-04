7ee5f8cc3ac30b96fd7ba471d44967ee50bc0f07
Aycan Ogut <aycanonfire@gmail.com>
2022-01-04 21:37:14 +0300

    create component

---
+++ src/components/Calculator/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Calculator.module.css';
+import Screen from '../Screen';
+import Buttons from '../Buttons';
+
+const Calculator = () => {
+  return (
+    <>
+      <Screen />
+      <Buttons />
+    </>
+  );
+};
+
+export default Calculator;
