97d560d462286f57cbdc861e6a409bc70397b009
Aycan Ogut <aycanonfire@gmail.com>
2022-01-06 14:43:36 +0300

    add dummy history functionality

--- src/components/History/index.tsx
+++ src/components/History/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import HistoryItem from '../HistoryItem';
 import Footer from '../Footer';
@@ -6,14 +6,26 @@ import Footer from '../Footer';
 import styles from './History.module.css';
 
 const History = () => {
+  const [history, setHistory] = useState([
+    { firstArgument: '1', operator: '+', secondArgument: '3', result: '2' } // hard coded dummy data
+  ]);
+
   return (
     <section className={styles.History}>
       <div className={styles.Histories}>
-        <HistoryItem firstArgument={21} operator="+" secondArgument={42} result={63} />
-        <HistoryItem firstArgument={21} operator="+" secondArgument={42} result={63} />
-        <HistoryItem firstArgument={21} operator="+" secondArgument={42} result={63} />
+        {history.map((item, index) => {
+          return (
+            <HistoryItem
+              key={index}
+              firstArgument={item.firstArgument}
+              operator={item.operator}
+              secondArgument={item.secondArgument}
+              result={item.result}
+            />
+          );
+        })}
       </div>
-      <Footer />
+      <Footer setHistory={setHistory} />
     </section>
   );
 };
