4df8e99afff2c6a2c20469b97d3ff250e84ed198
Aycan Ogut <aycanonfire@gmail.com>
2022-01-06 14:42:02 +0300

    remove styles import

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import Screen from '../Screen';
 import Buttons from '../Buttons';
 
-import styles from './Calculator.module.css';
-
 const Calculator = () => {
   return (
     <>
