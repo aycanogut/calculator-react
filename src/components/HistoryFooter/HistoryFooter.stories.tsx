7999694107bfa584ac1e0a94959dc2cb5a76de9c
Aycan Ogut <aycanonfire@gmail.com>
2022-01-14 10:22:47 +0300

    update component name and prop declaring

--- src/components/HistoryFooter/HistoryFooter.stories.tsx
+++ src/components/HistoryFooter/HistoryFooter.stories.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import Footer from '.';
+import HistoryFooter, { Props } from '../HistoryFooter';
 
 import '../../styles/global.css';
 
 export default {
   title: 'Footer',
-  component: Footer
+  component: HistoryFooter
 };
 
-export const FooterStory = () => <Footer />;
+export const FooterStory = (args: JSX.IntrinsicAttributes & Props) => <HistoryFooter {...args} />;
