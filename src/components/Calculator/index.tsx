d7fe01ae54ccc1445548f13c08086102e0ab1194
Aycan Ogut <aycanonfire@gmail.com>
2022-01-15 13:50:31 +0300

    add wrapper styles

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import Screen from '../Screen';
 import Buttons from '../Buttons';
 
+import styles from './Calculator.module.css';
+
 const Calculator = () => {
   return (
-    <>
+    <div className={styles.Calculator}>
       <Screen />
       <Buttons />
-    </>
+    </div>
   );
 };
 
