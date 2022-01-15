a4fe48b40d71ca16b1156d0061a8d468f1a0958c
Aycan Ogut <aycanonfire@gmail.com>
2022-01-15 13:50:21 +0300

    remove wrapper styles

--- src/components/Calculator/Calculator.stories.tsx
+++ src/components/Calculator/Calculator.stories.tsx
@@ -3,15 +3,10 @@ import React from 'react';
 import Calculator from '../Calculator';
 
 import '../../styles/global.css';
-import styles from './Calculator.module.css';
 
 export default {
   title: 'Calculator',
   component: Calculator
 };
 
-export const CalculatorStory = () => (
-  <div className={styles.CalculatorWrapper}>
-    <Calculator />
-  </div>
-);
+export const CalculatorStory = () => <Calculator />;
