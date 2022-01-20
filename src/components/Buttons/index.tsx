f4359a8af785e2e78515ffafeb2a87d506149935
Aycan Ogut <aycanonfire@gmail.com>
2022-01-20 13:44:40 +0300

    add onClick events

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -6,38 +6,73 @@ import Button from '../Button';
 
 import styles from './Buttons.module.css';
 
-interface UpdateDisplayValues {
-  e: MouseEvent;
-  target: any;
-}
-
 const Buttons = ({ displayValue, setDisplayValue }: DisplayValues) => {
-  const updateDisplay = (e: UpdateDisplayValues) => {
+  const updateDisplay = (e: any) => {
     setDisplayValue(displayValue.concat(e.target.textContent));
   };
 
   return (
     <section className={styles.Buttons}>
-      <Button variant="operator">C</Button>
-      <Button variant="operator">⌫</Button>
-      <Button variant="operator">%</Button>
-      <Button variant="operator">÷</Button>
-      <Button variant="number">7</Button>
-      <Button variant="number">8</Button>
-      <Button variant="number">9</Button>
-      <Button variant="operator">&times;</Button>
-      <Button variant="number">4</Button>
-      <Button variant="number">5</Button>
-      <Button variant="number">6</Button>
-      <Button variant="operator">-</Button>
-      <Button variant="number">1</Button>
-      <Button variant="number">2</Button>
-      <Button variant="number">3</Button>
-      <Button variant="operator">+</Button>
-      <Button variant="number">+/-</Button>
-      <Button variant="number">0</Button>
-      <Button variant="number">.</Button>
-      <Button variant="equal">=</Button>
+      <Button variant="operator" onClick={updateDisplay}>
+        C
+      </Button>
+      <Button variant="operator" onClick={updateDisplay}>
+        ⌫
+      </Button>
+      <Button variant="operator" onClick={updateDisplay}>
+        %
+      </Button>
+      <Button variant="operator" onClick={updateDisplay}>
+        ÷
+      </Button>
+      <Button variant="number" onClick={updateDisplay}>
+        7
+      </Button>
+      <Button variant="number" onClick={updateDisplay}>
+        8
+      </Button>
+      <Button variant="number" onClick={updateDisplay}>
+        9
+      </Button>
+      <Button variant="operator" onClick={updateDisplay}>
+        &times;
+      </Button>
+      <Button variant="number" onClick={updateDisplay}>
+        4
+      </Button>
+      <Button variant="number" onClick={updateDisplay}>
+        5
+      </Button>
+      <Button variant="number" onClick={updateDisplay}>
+        6
+      </Button>
+      <Button variant="operator" onClick={updateDisplay}>
+        -
+      </Button>
+      <Button variant="number" onClick={updateDisplay}>
+        1
+      </Button>
+      <Button variant="number" onClick={updateDisplay}>
+        2
+      </Button>
+      <Button variant="number" onClick={updateDisplay}>
+        3
+      </Button>
+      <Button variant="operator" onClick={updateDisplay}>
+        +
+      </Button>
+      <Button variant="number" onClick={updateDisplay}>
+        +/-
+      </Button>
+      <Button variant="number" onClick={updateDisplay}>
+        0
+      </Button>
+      <Button variant="number" onClick={updateDisplay}>
+        .
+      </Button>
+      <Button variant="equal" onClick={updateDisplay}>
+        =
+      </Button>
     </section>
   );
 };
