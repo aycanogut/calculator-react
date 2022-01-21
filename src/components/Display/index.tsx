f0655368fe32feaf8aaaa61031f15b549e8a674f
Aycan Ogut <aycanonfire@gmail.com>
2022-01-21 13:51:39 +0300

    add conditional font-size functionality

--- src/components/Display/index.tsx
+++ src/components/Display/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import cx from 'classnames';
 
 import styles from './Display.module.css';
 
@@ -7,7 +8,15 @@ export interface DisplayProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 const Display = ({ displayValue }: DisplayProps) => {
-  return <div className={styles.Display}>{displayValue}</div>;
+  const [fontSize, setFontSize] = useState(false);
+
+  useEffect(() => {
+    displayValue.length < 10 ? setFontSize(true) : setFontSize(false);
+  }, [displayValue]);
+
+  return (
+    <div className={cx(styles.Display, !fontSize ? styles.SmallFont : '')}>{displayValue}</div>
+  );
 };
 
 export default Display;
