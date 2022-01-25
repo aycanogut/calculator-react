bae425bc1c2011897bd717baa72eefe8bf6f56d4
Aycan Ogut <aycanonfire@gmail.com>
2022-01-25 14:14:58 +0300

    add displayable characters list

---
+++ src/utils/displayableChars.tsx
@@ -0,0 +1,21 @@
+const displayableChars = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '÷',
+  'x',
+  '-',
+  '+',
+  '.'
+];
+
+const chars = displayableChars.map((char) => char);
+
+export { chars };
