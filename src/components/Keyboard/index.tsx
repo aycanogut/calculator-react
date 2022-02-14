1aef83c7a0f21d16fe0672044cbf5ddbcf2cf7fc
Aycan Ogut <aycanonfire@gmail.com>
2022-02-14 16:50:44 +0300

    add array of button types

--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -82,8 +82,8 @@ const Keyboard = ({
     { variant: 'operator', value: '=', action: handleOperator }
   ];
 
-  const operators = ['+', '-', 'x', '÷', 'C', '⌫', '%', '±', '.', '='];
-  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const operators: string[] = ['+', '-', 'x', '÷', 'C', '⌫', '%', '±', '.', '='];
+  const numbers: string[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
   return (
     <section className={styles.Keyboard}>
