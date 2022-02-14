bfd135574e7b70b161e28667cafcc4b3588f9bed
Aycan Ogut <aycanonfire@gmail.com>
2022-02-14 23:49:26 +0300

    remove button arrays and import

--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { getButtonType } from '../../utils/getButtonType';
-
 import Button from '../Button';
 
 import styles from './Keyboard.module.css';
@@ -83,10 +81,6 @@ const Keyboard = ({
     { variant: 'operator', value: '.', action: undefined },
     { variant: 'equal', value: '=', action: handleOperator }
   ];
-
-  const operators: string[] = ['+', '-', 'x', '÷', 'C', '⌫', '%', '±', '.'];
-  const numbers: string[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  const equal: string[] = ['='];
 
   return (
     <section className={styles.Keyboard}>
