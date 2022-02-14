3eb6869a24bd6356ea04fc064976049f85ca3498
Aycan Ogut <aycanonfire@gmail.com>
2022-02-14 23:48:33 +0300

    remove button arrays

--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -84,10 +84,6 @@ const Keyboard = ({
     { variant: 'equal', value: '=', action: handleOperator }
   ];
 
-  const operators: string[] = ['+', '-', 'x', '÷', 'C', '⌫', '%', '±', '.'];
-  const numbers: string[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  const equal: string[] = ['='];
-
   return (
     <section className={styles.Keyboard}>
       {buttonsArray.map((btn, id) => (
