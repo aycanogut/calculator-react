97c3b43b5672ee52038e4c763779f021bfe8bc9f
Aycan Ogut <aycanonfire@gmail.com>
2022-02-14 21:03:24 +0300

    update regex

--- src/utils/getButtonType.tsx
+++ src/utils/getButtonType.tsx
@@ -1,6 +1,6 @@
 export const getButtonType = (button: string) => {
   const numbers = new RegExp(/\d+/);
-  const operators = new RegExp(/[+-x÷C⌫%±.=]/);
+  const operators = new RegExp(/([C+x÷⌫%±.-])/);
 
   if (numbers.test(button)) {
     return 'number';
