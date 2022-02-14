161edba9d767a9c4aa2c53af3a76fe95f05c58e4
Aycan Ogut <aycanonfire@gmail.com>
2022-02-14 16:11:02 +0300

    add arrays of button values

--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -82,6 +82,9 @@ const Keyboard = ({
     { variant: 'equal', value: '=', action: handleOperator }
   ];
 
+  const operators = ['+', '-', 'x', '÷', 'C', '⌫', '%', '+/-', '.', '='];
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
   return (
     <section className={styles.Keyboard}>
       {buttonsArray.map((btn, id) => (
